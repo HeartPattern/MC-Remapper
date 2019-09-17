737f05245c7500b343de7ac37bcd50d99fca2839
ReadyMadeProgrammer <jun2620354@kaist.ac.kr>
2019-09-17 17:45:48 +0900

    Remove debug local variable info.
    Some decompiler does not work with debug variable info.

--- src/main/kotlin/io/github/readymadeprogrammer/mcremapper/SimpleClassRemapper.kt
+++ src/main/kotlin/io/github/readymadeprogrammer/mcremapper/SimpleClassRemapper.kt
@@ -1,7 +1,10 @@
 package io.github.readymadeprogrammer.mcremapper
 
 import org.objectweb.asm.ClassVisitor
+import org.objectweb.asm.Label
+import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.commons.ClassRemapper
+import org.objectweb.asm.commons.MethodRemapper
 import org.objectweb.asm.commons.Remapper
 
 // Also remap inner class name
@@ -17,5 +20,18 @@ class SimpleClassRemapper(cv: ClassVisitor, remapper: Remapper) : ClassRemapper(
             remapped.substring(outest + 1, remapped.length),
             access
         )
+    }
+
+    override fun createMethodRemapper(mv: MethodVisitor): MethodVisitor {
+        return LocalVarFixMethodRemapper(mv, remapper)
+    }
+}
+
+class LocalVarFixMethodRemapper(mv: MethodVisitor, remapper: Remapper) : MethodRemapper(mv, remapper) {
+    override fun visitLocalVariable(name: String?, desc: String?, signature: String?, start: Label?, end: Label?, index: Int) {
+        if (name != null && name == "\u2603");
+//            super.visitLocalVariable("debug$index", desc, signature, start, end, index)
+        else
+            super.visitLocalVariable(name, desc, signature, start, end, index)
     }
 }
