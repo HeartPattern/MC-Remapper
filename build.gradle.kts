b837c1d4fb5cc36ae1c224d6b1472f15aca85513
J3fftw <44972470+J3fftw1@users.noreply.github.com>
2024-01-18 16:24:46 +0100

    updated to 1.20 (#29)
    * updated to 1.20
    
    * update kotlin to java 20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.7.0"
+    kotlin("jvm") version "1.9.22"
     application
     `maven-publish`
 }
@@ -14,9 +14,9 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("com.github.ajalt.clikt", "clikt", "3.0.1")
-    implementation("org.ow2.asm", "asm", "9.3")
-    implementation("org.ow2.asm", "asm-commons", "9.3")
-    implementation("org.ow2.asm", "asm-tree", "9.3")
+    implementation("org.ow2.asm", "asm", "9.6")
+    implementation("org.ow2.asm", "asm-commons", "9.6")
+    implementation("org.ow2.asm", "asm-tree", "9.6")
     implementation("me.tongfei", "progressbar", "0.9.3")
     implementation("io.heartpattern","mcversions","1.0.3-SNAPSHOT")
 
@@ -26,14 +26,14 @@ dependencies {
 }
 
 application {
-    mainClassName = "io.heartpattern.mcremapper.commandline.MCRemapperAppKt"
+    mainClass.set( "io.heartpattern.mcremapper.commandline.MCRemapperAppKt" )
 }
 
 tasks {
     compileKotlin {
         kotlinOptions {
             freeCompilerArgs = listOf("-progressive")
-            jvmTarget = "17"
+            jvmTarget = "20"
         }
     }
 
